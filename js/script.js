b6e39fe58ac164e189c3770eab85a3401d65c9b4
Oliver <draper87@gmail.com>
2020-06-16 18:31:07 +0200

    6

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
   $(document).on('click', '.send i',
-    function() {
+    function scriviMessaggio() {
       var template = $('.tonotshow .messaggio').clone();
       var mioValore = $('.text input').val();
       var orario = new Date();
@@ -12,9 +12,23 @@ $(document).ready(function() {
       template.find('span').text(ora + ':' + minuti);
       template.addClass('greeny');
       $('.main-chat').append(template);
-      // $('.main-chat').append('<div class="template greeny"><p class="greeny">' + mioValore + '<span>' + ora + ':' + minuti + '</span></p></div>');
       $('input').val('');
     }
   )
+
+  $('.text input').keypress(function(event) {
+    if (event.which === 13 ) {
+      var template = $('.tonotshow .messaggio').clone();
+      var mioValore = $('.text input').val();
+      var orario = new Date();
+      var ora = orario.getHours();
+      var minuti = orario.getMinutes();
+      template.children('p').prepend(mioValore);
+      template.find('span').text(ora + ':' + minuti);
+      template.addClass('greeny');
+      $('.main-chat').append(template);
+      $('input').val('');
+    }
+  })
 
 })
